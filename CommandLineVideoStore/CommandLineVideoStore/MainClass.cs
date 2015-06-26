8b2c3b7d48243017b8450e8d4be3616c149caf45
dwy <dominic.wyler@zuehlke.com>
2015-06-26 15:21:11 +0200

    introduced MyConsole

--- CommandLineVideoStore/CommandLineVideoStore/MainClass.cs
+++ CommandLineVideoStore/CommandLineVideoStore/MainClass.cs
@@ -8,10 +8,9 @@ namespace CommandLineVideoStore
 
     public class MainClass
     {
-        private readonly TextReader _in;
-        private readonly TextWriter _out;
         private readonly MovieRepository movieRepository;
-        private readonly RentalFactory rentalFactory;
+
+        private readonly MyConsole console;
 
         public static void Main()
         {
@@ -21,57 +20,22 @@ namespace CommandLineVideoStore
 
         public MainClass(TextReader @in, TextWriter @out)
         {
-            _out = @out;
-            _in = @in;
-            this.movieRepository = new MovieRepository();
-            this.rentalFactory = new RentalFactory(movieRepository);
+            movieRepository = new MovieRepository();
+            var rentalFactory = new RentalFactory(movieRepository);
+            console = new MyConsole(@in, @out, rentalFactory);
         }
 
         public void Run()
         {
-            // read movies from file
-            foreach (Movie movie in this.movieRepository.GetAll())
-            {
-                this._out.WriteLine(movie.Key + ": " + movie.Name);
-            }
+            console.PrintMovies(movieRepository.GetAll());
 
-            _out.Write("Enter customer name: ");
-            string customerName = _in.ReadLine();
+            string customerName = console.InputCustomerName();
 
-            _out.WriteLine("Choose movie by number followed by rental days, just ENTER for bill:");
-
-            List<Rental> rentals = this.InputRentals();
+            List<Rental> rentals = console.InputRentals();
             var rentalRecord = new RentalRecord(customerName, rentals);
-            string result = "Rental Record for " + rentalRecord.CustomerName + "\n";
-
-            foreach (var rental in rentalRecord.Rentals)
-            {
-                // show figures for this rental
-                result += "\t" + rental.GetMovieName() + "\t" + rental.GetAmount().ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-            }
-
-            // add footer lines
-            result += "You owed " + rentalRecord.GetTotalAmount().ToString("0.0", CultureInfo.InvariantCulture) + "\n";
-            result += "You earned " + rentalRecord.GetFrequentRenterPoints() + " frequent renter points\n";
-
-            _out.Write(result);
-        }
-
-        private List<Rental> InputRentals()
-        {
-            var rentals = new List<Rental>();
-            while (true)
-            {
-                string input = this._in.ReadLine();
-                if (string.IsNullOrEmpty(input))
-                {
-                    break;
-                }
-                string[] rentalData = input.Split(' ');
-                Rental rental = this.rentalFactory.CreateFrom(rentalData);
-                rentals.Add(rental);
-            }
-            return rentals;
+            
+            console.PrintRentalRecord(rentalRecord);
+            console.PrintFooter(rentalRecord);
         }
     }
 }
